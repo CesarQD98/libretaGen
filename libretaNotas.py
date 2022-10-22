388b8f0687648adc8eef6571bfb22faa17f5dcb4
Rico98 <asd.cqd98@gmail.com>
2022-10-22 18:07:58 -0500

    Added all the courses into the 'fill in' process
    Added the capability to store the courses defined in a list CURSOSAPTITUDES

--- libretaNotas.py
+++ libretaNotas.py
@@ -1,3 +1,4 @@
+from operator import index
 import openpyxl
 import pprint
 
@@ -7,54 +8,45 @@ wb = openpyxl.load_workbook('./data/notas.xlsx')
 sheet = wb['1°E']
 alumnoData = {}
 
+# TODO: Create a dict or list that contains every course and its aptitudes
 GRADO = sheet['B3'].value.strip('°')
 SECCION = sheet['B5'].value.upper()
-CURSOSAPTITUDES = ['Desarrollo Personal, Ciudadanía y Cívica', ['Construye su identidad', 'Convive y participa democráticamente en la búsqueda del bien común'], 'Ciencias Sociales', [
-    'Construye interpretaciones históricas', 'Gestiona responsablemente el espacio y el ambiente', 'Gestiona responsablemente los recursos económicos']]
+CURSOSAPTITUDES = {'Desarrollo Personal, Ciudadanía y Cívica': ['Construye su identidad', 'Convive y participa democráticamente en la búsqueda del bien común'], 'Ciencias Sociales': [
+    'Construye interpretaciones históricas', 'Gestiona responsablemente el espacio y el ambiente', 'Gestiona responsablemente los recursos económicos']}
 
-# TODO: Fill in alumnoData with each student info.
+# Fill in alumnoData with each student info.
 print('Reading rows...')
-row = 10
+fila = 10
 while (True):
-    name = sheet['B' + str(row)].value
+    notasCol = 3
+    name = sheet['B' + str(fila)].value
 
     if name == None:
         break
 
     aptitud = sheet['C9'].value
-    nota = sheet['C' + str(row)].value
-    nroOrden = row - 9
+    nota = sheet['C' + str(fila):'G' + str(fila)]
+    nroOrden = fila - 9
 
     # Adds a new student to the dict
     alumnoData.setdefault(name, {})
 
-    # TODO: Adds 'notas', more than course.
     # Adds 'grado', 'seccion', 'nroOrden' and 'cursos'
     alumnoData[name].setdefault('grado', GRADO)
     alumnoData[name].setdefault('seccion', SECCION)
     alumnoData[name].setdefault('nroOrden', nroOrden)
     alumnoData[name].setdefault('cursos', {})
 
-    for curso in CURSOS:
-        alumnoData[name]['cursos'].setdefault(curso, {})
-
-    # alumnoData[name]['cursos'][curso].setdefault(aptitud, nota)
-
-    print(row)
-    row += 1
+    for k, v in CURSOSAPTITUDES.items():
+        alumnoData[name]['cursos'].setdefault(k, {})
+        for i in range(len(v)):
+            alumnoData[name]['cursos'][k].setdefault(v[i], sheet.cell(row=fila, column=(notasCol + i)).value)
+        
+    fila += 1
 
 
 # Expected output for each student:
 # alumnoData = {'AGUIRRE PRADO...': {'grado': 1, 'seccion': 'e',
 #                                    'cursos': {'desarrollo...': {'aptitud1': 'A', 'aptitud2': 'B'}}}}
 
-
-# print('B47: ' + str(sheet['B47'].value))
-# print('C8: ' + sheet['C8'].value)
-# print('D8: ' + sheet['C8'].value)
-# print('GRADO: ' + str(GRADO))
-# print('SECCION: ' + str(SECCION))
-# print('max_row: ' + str(sheet.max_row))
-# print('max_column: ' + str(sheet.max_column))
-# print()
 print(pprint.pformat(alumnoData))
